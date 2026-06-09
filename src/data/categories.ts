11cd2d58c64afde5c613f619764142b3616b100e
firgiahmadsuryaman <firgi_ahmad_suryaman@teknokrat.ac.id>
2026-06-09 21:08:42 +0700

    membuat berkas data mock awal kategori

---
+++ src/data/categories.ts
@@ -0,0 +1,11 @@
+import { Category } from "@/types";
+
+export const initialCategories: Category[] = [
+  { id: "1", name: "Pantai", totalDestinations: 45 },
+  { id: "2", name: "Gunung", totalDestinations: 28 },
+  { id: "3", name: "Air Terjun", totalDestinations: 15 },
+  { id: "4", name: "Danau", totalDestinations: 8 },
+  { id: "5", name: "Budaya & Sejarah", totalDestinations: 12 },
+  { id: "6", name: "Kuliner", totalDestinations: 30 },
+  { id: "7", name: "Alam", totalDestinations: 18 }
+];
