aec9ce98605bccc1572aa09eb3831c82ea7e3298
firgiahmadsuryaman <firgi_ahmad_suryaman@teknokrat.ac.id>
2026-06-09 21:09:34 +0700

    menambahkan fungsi hapus destinasi ke dalam app context

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -123,7 +123,11 @@ export function AppProvider({ children }: { children: React.ReactNode }) {
     setDestinations(updated);
     localStorage.setItem("sb_destinations", JSON.stringify(updated));
   };
-  const deleteDestination = (id: string) => {};
+  const deleteDestination = (id: string) => {
+    const updated = destinations.filter((dest) => dest.id !== id);
+    setDestinations(updated);
+    localStorage.setItem("sb_destinations", JSON.stringify(updated));
+  };
   const addCategory = (name: string) => {};
   const updateCategory = (id: string, name: string) => {};
   const deleteCategory = (id: string) => {};
